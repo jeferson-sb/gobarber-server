5c6e7fcd2e11220f88e85d685f9988adfe57077f
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-09-01 22:33:06 -0300

    feat: create forgot password service and provider

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUsersRepository,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.userRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(checkUserExists.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
