1a5b69c8889fd19d24d342b371eac65e30b1d33e
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-09-03 22:11:56 -0300

    refactor: service specs to use beforeEach

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,12 +2,16 @@ import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRep
 import CreateAppointmentService from './CreateAppointmentService';
 import AppError from '@shared/errors/AppError';
 
+let fakeAppointmentRepository: FakeAppointmentRepository;
+let createAppointment: CreateAppointmentService;
+
 describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository();
+    createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+  });
+
   it('should be able to create a new appointment', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
     const appointment = await createAppointment.execute({
       datetime: new Date(),
       providerId: '123123123',
@@ -18,10 +22,6 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments at the same time', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
     const appointmentDate = new Date(2020, 7, 31, 11);
 
     await createAppointment.execute({
