9f917e65db25a6449b3b62051258735c90197bec
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-09-12 16:14:15 -0300

    feat: create provider day and month availability controller

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ProviderController from '../controllers/ProvidersController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const router = Router();
 const providersController = new ProviderController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 router.use(ensureAuthenticated);
 
 router.get('/', providersController.index);
+router.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+router.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default router;
