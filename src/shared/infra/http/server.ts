2eb6a13ae0b0c1c3280f74d42bf728d81e9f3834
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-09-13 18:16:32 -0300

    setup: rate limiter middleware

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,9 +10,11 @@ import '@shared/infra/typeorm';
 import '@shared/container';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
